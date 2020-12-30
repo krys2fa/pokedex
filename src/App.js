b4dfc6773b0b4f6c835ff3c3c716ceb244dc277c
Christopher Amanor <kriskit7@gmail.com>
2020-12-30 04:01:10 +0000

    Setup GraphQL

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { PokemonsContainer } from './containers/PokemonsContainer';
+
 export function App() {
+  const client = new ApolloClient({
+    uri: 'https://graphql-pokemon.now.sh'
+  });
+
   return (
-    <p>I am a Pokemon</p>
+    <ApolloProvider client={client}>
+      <main>
+        <PokemonsContainer />
+      </main>
+
+    </ApolloProvider>
 	)
 }
---
+++ src/containers/PokemonsContainer.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+
+export function PokemonsContainer() {
+  return (<p> I am a Pokemon container </p>);
+}
